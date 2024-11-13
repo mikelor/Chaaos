b8a81ad58fa4ebfaec3c7a0c7cce0d4675adc2b0
mikelor <mikelor@gmail.com>
2024-11-13 10:49:53 -0800

    Updating for .NET9

--- src/Chaaos/Platforms/Android/Auto/Models/PlaceInfo.cs
+++ src/Chaaos/Platforms/Android/Auto/Models/PlaceInfo.cs
@@ -5,12 +5,12 @@ namespace Chaaos.Platforms.Android.Auto.Models;
 
 public class PlaceInfo
 {
-    public string Title { get; set; }
-    public string Address { get; set; }
-    public string Description { get; set; }
-    public string PhoneNumber { get; set; }
-    public Location Location { get; set; }
-    public  PlaceMarker Marker { get; set; }
+    public required string Title { get; set; }
+    public required string Address { get; set; }
+    public required string Description { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required Location Location { get; set; }
+    public required PlaceMarker Marker { get; set; }
 
     public static Location CreateLocation(double latitude, double longitude)
     {
